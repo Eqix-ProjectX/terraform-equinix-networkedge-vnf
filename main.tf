4c570f41c4abb2abeb62722d40c6809d5d40b7cd
EQIX-TAM-JP <snakayama@ap.equinix.com>
2024-06-13 14:27:53 +0900

    iosxe

--- main.tf
+++ main.tf
@@ -36,3 +36,54 @@ resource "equinix_network_device" "c8kv-ha" {
     key_name        = var.key_name
   }
 }
+
+resource "iosxe_interface_ethernet" "to_metal" {
+  type                           = "GigabitEthernet"
+  name                           = "3"
+  bandwidth                      = 1000000
+  description                    = "linktometal"
+  shutdown                       = false
+  ip_proxy_arp                   = false
+  ip_redirects                   = false
+  ip_unreachables                = false
+  ipv4_address                   = "192.168.0.2"
+  ipv4_address_mask              = "255.255.255.252"
+  # ip_dhcp_relay_source_interface = "Loopback100"
+  # ip_access_group_in             = "1"
+  # ip_access_group_in_enable      = true
+  # ip_access_group_out            = "1"
+  # ip_access_group_out_enable     = true
+  # helper_addresses = [
+  #   {
+  #     address = "10.10.10.10"
+  #     global  = false
+  #     vrf     = "VRF1"
+  #   }
+  # ]
+  # source_template = [
+  #   {
+  #     template_name = "TEMP1"
+  #     merge         = false
+  #   }
+  # ]
+  ipv6_enable             = false
+  # ipv6_mtu                = 1300
+  # ipv6_nd_ra_suppress_all = true
+  # ipv6_address_dhcp       = true
+  # ipv6_link_local_addresses = [
+  #   {
+  #     address    = "fe80::9656:d028:8652:66b6"
+  #     link_local = true
+  #   }
+  # ]
+  # ipv6_addresses = [
+  #   {
+  #     prefix = "2001:DB8::/32"
+  #     eui_64 = true
+  #   }
+  # ]
+  arp_timeout             = 300
+  spanning_tree_link_type = "point-to-point"
+  # service_policy_input    = "POLICY1"
+  # service_policy_output   = "POLICY1"
+}
